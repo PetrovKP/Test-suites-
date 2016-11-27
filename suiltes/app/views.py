0d43eff4543a947049cd18efe33d3a644bf1bf77
Kirill <Petrovich_1811@mail.ru>
2016-11-27 13:54:57 +0300

    Добавлена стартова страничка вывода тестов

--- suiltes/app/views.py
+++ suiltes/app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.template import loader
+
+from django.http import HttpResponse
+from .models import Test
+
+import datetime
 
 # Create your views here.
+
+def index(request):
+    tests_name = [obj.name for obj in Test.objects.all()]
+    return render(request, "index.html", {"list_test": tests_name})
--- suiltes/suiltes/settings.py
+++ suiltes/suiltes/settings.py
@@ -18,6 +18,9 @@ pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -59,7 +62,9 @@ ROOT_URLCONF = 'suiltes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
