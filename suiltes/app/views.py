34fdd6055a37d79eb8bed3e844a67b85bd76af25
Kirill <Petrovich_1811@mail.ru>
2016-11-27 19:43:01 +0300

    Реализован метод добавления

--- suiltes/app/views.py
+++ suiltes/app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db import IntegrityError
 
 from .models import Test
 
@@ -14,8 +15,21 @@ def index(request):
     return render(request, "index.html", {"list_test": tests_name})
 
 
-# Запуск скрипты=а и вывод результов
+# Запуск скрипта и вывод результов
 def run(request):
     test = Test.objects.get(id=request.session['id'])
 
     return render(request, 'run.html', {"test": test.run})
+
+
+# Добавления теста
+def add(request):
+    if request.method == 'POST':
+        try:
+            test = Test(name = request.POST['name'], run = request.POST['srtingrun'])
+            test.save()
+            return render(request, "add.html", { "isAdd": "ok" })
+        except IntegrityError:
+            return render(request, "add.html", { "isAdd": "error" })
+
+    return render(request, "add.html")
