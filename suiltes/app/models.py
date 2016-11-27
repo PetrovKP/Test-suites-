c5a614d896209a62db3c310ccaee258a60db96d9
Kirill <Petrovich_1811@mail.ru>
2016-11-27 19:42:36 +0300

    Сделано имя уникальным

--- suiltes/app/models.py
+++ suiltes/app/models.py
@@ -4,6 +4,6 @@ from django.db import models
 
 
 class Test(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=20)
-    run = models.CharField(max_length=40)
+    id = models.AutoField(primary_key = True)
+    name = models.CharField(max_length = 20, unique = True)
+    run = models.CharField(max_length = 40)
