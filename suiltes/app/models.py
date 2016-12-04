db39dee74c9bffdc0c4d47290f666f3493f62daa
Kirill <Petrovich_1811@mail.ru>
2016-12-05 00:36:43 +0300

    Добавлены модель скиптов и их реализация

--- suiltes/app/models.py
+++ suiltes/app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+
+from django import forms
 # Create your models here.
 
 
@@ -7,3 +9,8 @@ class Test(models.Model):
     id = models.AutoField(primary_key = True)
     name = models.CharField(max_length = 20, unique = True)
     run = models.CharField(max_length = 40)
+
+
+class UploadScriptForm(forms.Form):
+    title = forms.CharField(label='Название',max_length=50)
+    script = forms.FileField(label='Скрипт')
