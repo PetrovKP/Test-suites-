4c06f08971c251c1dca99e6f73519b33c12653ca
Kirill <Petrovich_1811@mail.ru>
2016-12-10 21:12:57 +0300

    Добавлены примитивные тесты на добаление и удаление

--- suiltes/app/tests.py
+++ suiltes/app/tests.py
@@ -4,3 +4,37 @@ from django.test import TestCase
 
 from .models import Test
 
+
+class SuitesTest(TestCase):
+
+    def test_index(self):
+        """Успешное открытие стартовое страницы"""
+        resp = self.client.get('')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_add(self):
+        """Проверка добавления теста"""
+        resp = self.client.post('/add/', {'name': '', 'run': ''})
+        self.assertEqual(resp.status_code, 200)
+
+    def test_add_is_object_in_db(self):
+        """Проверка нахождения добавленного теста в бд"""
+        resp = self.client.post('/add/', {'name': 'Тест1', 'run': 'python2 run_test.py'})
+
+        test = Test.objects.get(name = 'Тест1')
+        self.assertEqual(test.run, 'python2 run_test.py')
+
+    def test_delete (self):
+        """Проверка удаления теста"""
+        resp = self.client.post('/delete/', {'name': '', 'run': ''})
+
+        self.assertEqual(resp.status_code, 200)
+
+    def test_del_is_object_in_db (self):
+        """Проверка удаления добавленного теста из бд"""
+        resp = self.client.post('/add/', { 'name': 'Тест1', 'run': 'python2 run_test.py' })
+
+        resp = self.client.post('/delete/', {'name': 'Тест1', 'run': 'python2 run_test.py'})
+
+        tests = Test.objects.all()
+        self.assertFalse(tests)
